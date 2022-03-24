ad83a735f375d060a999b55450944f89edbcad5f
Joshua Cheung <contact.cjoshua@gmail.com>
2022-03-23 22:15:42 -0700

    New hello function

--- hello.js
+++ hello.js
@@ -4,11 +4,27 @@
   
 //   sayHello();
 
-const sayHello  = function (name) {
+// const sayHello  = function (name) {
+//     console.log("Hello, " + name);
+//     }
+
+
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+const sayHelloToConsole  = function (name) {
     console.log("Hello, " + name);
-    }
+  }
+  sayHelloToConsole('John'); 
 
+//   const returnSayHello  = function (name) {
+//     return "Hello, " + name;
+//   }
+//   const greeting = returnSayHello('John');
 
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+  const returnSayHello  = function (name) {
+    return "Hello, " + name;
+  }
+  const greeting = returnSayHello('John');
+  console.log(greeting);
